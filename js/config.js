1bb448f8cdc7e64baaf08855fb9d4978b002ce2c
mariaeugeniaurriste-droid <mariaeugeniaurriste@gmail.com>
2025-08-28 11:25:19 -0700

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,22 @@
+export default {
+  GAS_ENDPOINT: "https://script.google.com/macros/s/AKfcybwaQ-qrLWHHBBQ6XC6m-6BkBtLo_x37dEaZCD4s6FwHtvJJLK_XflFbLsRXH2oVTw/exec",
+  EMPLOYEE_WA: "5493434515370",
+  TRANSFER: {
+    alias: "abuela.perla.mp",
+    titular: "Agustin Urriste",
+    compa: "5493434515370"
+  },
+  ENVIO: {
+    Diamante: 500,
+    Strobel: 800,
+    retiro: 0
+  },
+  HOURS: {
+    open: "19:00",
+    close: "23:59",
+    open_days: [0,1,3,4,5,6],
+    closed_dates: []
+  },
+  TOKEN_KEY: "AP_ADMIN_TOKEN",
+  ALLOW_NOTES: new Set(["Hamburguesas", "Lomitos", "Patynesas y Milanesas"])
+};
